b35a18607ff81902fa03569520b87298dfd850a0
lucasramostech_github <lucasramosstar66@gmail.com>
2026-05-29 17:13:00 -0300

    Adding the files

---
+++ script.js
@@ -0,0 +1,78 @@
+gsap.registerPlugin(ScrollTrigger);
+
+//Computadores e monitores padrões
+ScrollTrigger.matchMedia({
+    "(min-width: 769px)": function () {
+        var timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.two',
+                start: '0% 95%',
+                end: '40% 50%',
+                scrub: true,
+            },
+        });
+
+        timeline.to('#xbox360', {
+            top: '150%',
+            left: '10%',
+            rotation: 360,
+            ease: 'none',
+        });
+
+        timeline.to('#controle', {
+            top: '150%',
+            left: '32%',
+            rotation: 410,
+            ease: 'none',
+        }, "<");
+
+        timeline.to('#rewards', {
+            top: '150%',
+            left: '0%',
+            rotation: 310,
+            ease: 'none',
+        }, "<");
+    },
+
+    //Responsivo para celulares
+    "(max-width: 768px)": function () {
+        var timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.two',
+                start: '0% 95%',
+                end: '20% 50%',
+                scrub: true,
+            },
+        });
+
+        timeline.to('#xbox360', {
+            top: '130%',
+            left: '40%',
+            rotation: 360,
+            ease: 'none',
+            scale: 1.5,
+        });
+
+        timeline.to('#controle', {
+            top: '125%',
+            left: '75%',
+            rotation: 410,
+            ease: 'none',
+            scale: 2,
+        }, "<");
+
+        timeline.to('#rewards', {
+            top: '125%',
+            left: '10%',
+            rotation: 310,
+            ease: 'none',
+            scale: 2,
+        }, "<");
+    },
+});
+
+
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("scrolled", window.scrollY > 50);
+});
